6513770375c84888d92f16dff77f72e4614e302e
Yu <milica.m.simovic3@gmail.com>
2024-08-17 13:16:25 +0200

    db if exists

--- alembic/env.py
+++ alembic/env.py
@@ -1,8 +1,10 @@
 
 from logging.config import fileConfig
 from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from sqlalchemy import pool, create_engine, text
 from alembic import context
+from sqlalchemy.exc import OperationalError
+
 
 
 from database import Base  
@@ -16,9 +18,26 @@ if config.config_file_name is not None:
 
 target_metadata = Base.metadata
 
+def create_database_if_not_exists(url):
+    
+    db_name = url.split('/')[-1]
+    server_url = url.replace(f'/{db_name}', '')
+    engine = create_engine(server_url)
+    conn = engine.connect()
+    conn.execution_options(isolation_level="AUTOCOMMIT")
+    try:
+        conn.execute(text(f"CREATE DATABASE IF NOT EXISTS `{db_name}`"))
+        print(f"Database '{db_name}' created or already exists.")
+    except OperationalError as e:
+        print(f"Error creating database '{db_name}': {e}")
+    finally:
+        conn.close()
+
+
 def run_migrations_offline():
     """Izvrši migracije u 'offline' načinu rada."""
     url = config.get_main_option("sqlalchemy.url")
+    create_database_if_not_exists(url)
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -36,6 +55,8 @@ def run_migrations_online():
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
+    url = config.get_main_option("sqlalchemy.url")
+    create_database_if_not_exists(url)
 
     with connectable.connect() as connection:
         context.configure(connection=connection, target_metadata=target_metadata)
